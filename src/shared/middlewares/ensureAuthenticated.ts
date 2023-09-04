1a6187ec6d41393d538d0ec40e176a1cf61f3c91
Hélder Afonso <helderafonsoss@hotmail.com>
2023-09-04 03:14:30 -0300

    fix: Correção da tratativa de erro do verify do jwtwebtoken

--- src/shared/middlewares/ensureAuthenticated.ts
+++ src/shared/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,5 @@
 import { type Response, type Request, type NextFunction } from 'express'
-import { type JwtPayload, verify } from 'jsonwebtoken'
+import { type JwtPayload, verify, TokenExpiredError } from 'jsonwebtoken'
 
 import authConfig from '../config/auth'
 import AppError from 'shared/utils/AppError'
@@ -15,29 +15,39 @@ export default async function ensureAuthenticated(
   res: Response,
   next: NextFunction,
 ): Promise<void> {
-  const authHeader = req.headers.authorization
+  try {
+    const authHeader = req.headers.authorization
 
-  if (authHeader === undefined) throw new AppError('JWT token is missing.', 401)
+    if (authHeader === undefined)
+      throw new AppError('JWT token is missing.', 401)
 
-  const [, token] = authHeader.split(' ')
-  const decoded = verify(token, authConfig.jwt.secret)
+    const [, token] = authHeader.split(' ')
+    const decoded = verify(token, authConfig.jwt.secret)
 
-  const { sub, roleId } = decoded as Payload
+    const { sub, roleId } = decoded as Payload
 
-  const userRepository = typeORMConnection.getRepository(User)
+    const userRepository = typeORMConnection.getRepository(User)
 
-  const user = await userRepository.findOneBy({ id: sub })
+    const user = await userRepository.findOneBy({ id: sub })
 
-  if (user === null || user.deletedAt !== null)
-    throw new AppError('User doesn`t exist.', 401)
+    if (user === null || user.deletedAt !== null)
+      throw new AppError('User doesn`t exist.', 401)
 
-  if (typeof sub === 'string') {
-    req.user = {
-      id: sub,
-      roleId,
-      teamId: user.teamId,
+    if (typeof sub === 'string') {
+      req.user = {
+        id: sub,
+        roleId,
+        teamId: user.teamId,
+      }
     }
-  }
 
-  next()
+    next()
+  } catch (err) {
+    if (err instanceof TokenExpiredError) {
+      throw new AppError(
+        err.message.charAt(0).toUpperCase() + err.message.slice(1),
+      )
+    }
+    next(err)
+  }
 }
